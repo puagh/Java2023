6d721bb36be6af95daaf4b5af1b3735ae9da08b9
Hector Leon <puagh@outlook.com>
2023-07-11 23:28:18 -0600

    Terminando métodos útiles para Strings

--- HelloWorld/HelloWorld/src/EjemploStringMetodosArreglo.java
+++ HelloWorld/HelloWorld/src/EjemploStringMetodosArreglo.java
@@ -7,11 +7,16 @@ public class EjemploStringMetodosArreglo {
             System.out.println("arreglo[i] = " + arreglo[i]);
         }
 
-        System.out.println("trabalenguas = " + trabalenguas.split("a"));
-        for (byte i = 0; i < trabalenguas.length(); i++){
-            System.out.println("arreglo[i] = " + arreglo[i]);
+        String[] arreglo2 = trabalenguas.split("a");
+        for (byte i = 0; i < arreglo2.length; i++){
+            System.out.println("arregloTrabalenguas[i] = " + arreglo2[i]);
         }
 
-        String[] arreglo2 = trajalenguas.
+        String archivo = "prueba.archivo.pdf";
+        String[] archivoArr = archivo.split("\\.");
+        for (byte i =0; i < archivoArr.length; i++){
+            System.out.println("archivoArr[i] = " + archivoArr[i]);
+        }
+        System.out.println("archivoArr = " + archivoArr[(archivoArr.length) - 1]);
     }
 }
