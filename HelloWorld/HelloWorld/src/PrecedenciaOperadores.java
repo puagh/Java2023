d0836721dc6ed4fb1612996dc1b8a8c9f9c59eee
Hector Leon <puagh@outlook.com>
2023-07-25 23:16:31 -0600

    Agregando ejemplo para precedencia de operadores

---
+++ HelloWorld/HelloWorld/src/PrecedenciaOperadores.java
@@ -0,0 +1,13 @@
+public class PrecedenciaOperadores {
+    public static void main(String[] args) {
+        int i = 14;
+        int j = 8;
+        int k = 4;
+        
+        double promedio = (i + j + k) / 3d;
+        System.out.println("promedio = " + promedio);
+
+        System.out.println("Terminando operaciones");
+        
+    }
+}
